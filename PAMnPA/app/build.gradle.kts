e18978e4874cb8d317587d382e8f74665c52f161
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2025-04-02 14:37:42 +0200

    lab3(room dependencies)

--- PAMnPA/app/build.gradle.kts
+++ PAMnPA/app/build.gradle.kts
@@ -37,4 +37,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    //sqlite db
+    implementation(libs.roomRuntime)
+    annotationProcessor(libs.roomCompiler)
 }
